9da399e9c9a4719000cbc95655985c40659bc312
VSofia-1 <sofia.evasquez01@gmail.com>
2024-10-14 21:47:56 -0500

    Adding video-views structures

---
+++ src/main/java/culturemedia/exception/CultureMediaException.java
@@ -0,0 +1,8 @@
+package culturemedia.exception;
+
+//As{i se crea una excepci{on:
+public class CultureMediaException extends Exception{
+    public CultureMediaException(String message){
+        super(message);
+    }
+}
